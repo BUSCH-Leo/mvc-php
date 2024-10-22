42f9c2a48cdad33419f4cf0ba8151b598c27614e
BUSCH-Leo <buschleo58@gmail.com>
2024-10-22 09:19:47 +0200

    Base pour créer un template

--- blog/src/model.php
+++ blog/src/model.php
@@ -1,31 +1,17 @@
 <?php
 
-function dbConnect() {
-    try {
-        $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
-        return $database;
-    } catch (Exception $e) {
-        die('Erreur : '.$e->getMessage());
-    }
-}
-
 function getPosts() {
     $database = dbConnect();
-    
     $statement = $database->query(
-        "SELECT id, title, content, 
-        DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date 
-        FROM posts 
-        ORDER BY creation_date DESC 
-        LIMIT 0, 5"
+        "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts ORDER BY creation_date DESC LIMIT 0, 5"
     );
-    
     $posts = [];
-    while ($row = $statement->fetch()) {
+    while (($row = $statement->fetch())) {
         $post = [
             'title' => $row['title'],
             'french_creation_date' => $row['french_creation_date'],
             'content' => $row['content'],
+            'identifier' => $row['id'],
         ];
 
         $posts[] = $post;
@@ -36,17 +22,12 @@ function getPosts() {
 
 function getPost($identifier) {
     $database = dbConnect();
-    
     $statement = $database->prepare(
-        "SELECT id, title, content,
-        DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date 
-        FROM posts 
-        WHERE id = ?"
+        "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts WHERE id = ?"
     );
-
     $statement->execute([$identifier]);
-    $row = $statement->fetch();
 
+    $row = $statement->fetch();
     $post = [
         'title' => $row['title'],
         'french_creation_date' => $row['french_creation_date'],
@@ -56,28 +37,35 @@ function getPost($identifier) {
     return $post;
 }
 
-function getComments($identifier) {
+function getComments($identifier)
+{
     $database = dbConnect();
-    
     $statement = $database->prepare(
-        "SELECT id, author, comment,
-        DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date 
-        FROM comments 
-        WHERE post_id = ? 
-        ORDER BY comment_date DESC"
+        "SELECT id, author, comment, DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM comments WHERE post_id = ? ORDER BY comment_date DESC"
     );
-
     $statement->execute([$identifier]);
 
     $comments = [];
-    while ($row = $statement->fetch()) {
+    while (($row = $statement->fetch())) {
         $comment = [
             'author' => $row['author'],
             'french_creation_date' => $row['french_creation_date'],
             'comment' => $row['comment'],
         ];
+
         $comments[] = $comment;
     }
 
     return $comments;
+}
+
+function dbConnect()
+{
+    try {
+        $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+
+        return $database;
+    } catch(Exception $e) {
+        die('Erreur : '.$e->getMessage());
+    }
 }
